1c4d97f9aec2bbd32f902f8c31f155d99cab19ee
VladM <vdstories20@gmail.com>
2023-09-19 17:36:07 +0200

    publish repo

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import { GlobalStyles } from "./globalstyle.styled";
+
+export const App = () => {
+  return (
+      <div>
+        <h1>VibeStyle</h1>
+        <GlobalStyles></GlobalStyles> 
+      </div>
+  )
+};
