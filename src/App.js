b2599193a985c9f6246ac52ae61821721468c5c1
Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
2023-09-22 15:20:59 +0300

    push global etc

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import { GlobalStyles } from "./globalstyle.styled";
 import '../node_modules/modern-normalize/modern-normalize.css';
 import './font.css';
 
 export const App = () => {
   return (
-      <div>
-        <h1>VibeStyle</h1>
-        <GlobalStyles></GlobalStyles> 
-      </div>
-  )
+    <div>
+      <h1>VibeStyle</h1>
+    </div>
+  );
 };
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'App';
 import './index.css';
 import './font.css';
+import { GlobalStyles } from 'Globalstyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <GlobalStyles />
   </React.StrictMode>
 );
