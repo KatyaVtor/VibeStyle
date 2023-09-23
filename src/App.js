902dfdcc6ad2fad7e56742c213ef75afa9d40884
VladM <vdstories20@gmail.com>
2023-09-23 13:08:32 +0200

    additinal set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import '../node_modules/modern-normalize/modern-normalize.css';
+import './font.css';
 
 export const App = () => {
   return (
     <div>
       <h1>VibeStyle</h1>
+      
     </div>
   );
 };
--- src/Globalstyle.js
+++ src/Globalstyle.js
@@ -1,36 +1,19 @@
 import { createGlobalStyle } from 'styled-components';
-import 'modern-normalize';
 
 export const GlobalStyles = createGlobalStyle`
-body {
+* {
+  font-family: 'Inter';
   margin: 0;
   padding: 0;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-h1,
-h2,
-h3,
-h4,
-h5,
-h6,
-p {
-  margin: 0;
-  padding: 0;
-}
-ul,
-ol {
-  margin: 0;
-  padding: 0;
+  box-sizing: content-box;
+  text-decoration: none;
   list-style: none;
 }
+
 img {
   display: block;
   max-width: 100%;
   height: auto;
   object-fit: cover;
 }
-a {
-  text-decoration: none;
-  padding: 0;
-}`;
+`;
